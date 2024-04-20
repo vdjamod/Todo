2dcf98c4c86a5ea0587f4d4d6e49798608c2802b
Solanki Hardik <sh369915@gmail.com>
2024-04-20 17:18:01 +0530

    app.js Updeted

---
+++ app.js
@@ -0,0 +1,13 @@
+let submitBtn = document.getElementById("submitbtn");
+
+submitBtn.addEventListener("click", () => {
+  let work = document.getElementById("inputBox").value;
+  let ulList = document.getElementById("ulList");
+  let li = document.createElement("li");
+  li.innerHTML = work;
+  let deleteBtn = document.createElement("button");
+  deleteBtn.classList = "deleteBtn";
+  deleteBtn.innerText = "delete";
+  li.append(deleteBtn);
+  ulList.append(li);
+});
